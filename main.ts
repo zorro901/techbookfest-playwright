3f4fa24aeab42645c4dea5c07938e4571c77d156
zorro901 <zoro907@gmail.com>
2022-10-01 17:59:22 +0900

    Init

---
+++ main.ts
@@ -0,0 +1,9 @@
+import {webkit} from '@playwright/test'
+
+(async () => {
+    const browser = await webkit.launch()
+    const page = await browser.newPage()
+    await page.goto('http://whatsmyuseragent.org/')
+    await page.screenshot({ path: `example.png` })
+    await browser.close()
+})()
